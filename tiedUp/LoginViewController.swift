331f2407b0efaeaad5c8e93cbadba71dcd365f77
Daniel Torres <dtorres@4geeks.com.ve>
2017-02-14 23:31:43 -0300

    first commit

---
+++ tiedUp/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  tiedUp
+//
+//  Created by Daniel Torres on 2/14/17.
+//  Copyright © 2017 Danieltorres. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var stackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let loginButton = FBSDKLoginButton()
+        stackView.addArrangedSubview(loginButton)
+        
+        
+        if ((FBSDKAccessToken.current()) != nil) {
+            let alertController: UIAlertController = UIAlertController(title: "login", message: "you're already logged", preferredStyle: .alert)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
